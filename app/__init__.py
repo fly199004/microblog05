f9024b6d2f77a6264542e9e0f8f3be5ce05e20c4
fly199004 <414562979@qq.com>
2024-01-30 19:17:57 +0800

    chapter 11

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ import os
 from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -18,6 +19,7 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+bootstrap = Bootstrap()
 
 
 if not app.debug:
